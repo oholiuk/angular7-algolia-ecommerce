c5fba895893efb780861268f5ef6124eeb30ae79
Fernando Israel Villarreal Martinez <fvillarreal@domfinreal.corp>
2019-06-12 17:56:30 -0500

    Generate some models

---
+++ src/app/_core/models/index.ts
@@ -0,0 +1,5 @@
+export * from './album';
+export * from './artist';
+export * from './product';
+export * from './product-in-cart';
+export * from './track-image';
---
+++ src/app/_core/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    price: number;
+    brand: string;
+    name: string;
+    description: string;
+    tags: [];
+    artwork: [],
+    releaseYear: number;
+    objectID: string;
+  }
+  
