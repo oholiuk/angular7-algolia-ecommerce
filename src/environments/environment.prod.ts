b8b30f16dcfa4b20050fce6ce1a648b7989633b6
oholiuk <oleksandr.oholiuk@gmail.com>
2019-06-17 12:06:47 -0500

    Environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,12 @@ export const environment = {
   production: true,
   paypal: {
     client: 'AesIc2gjvYH0EtI_wEqNMDQCrQsJpZOa-P-QwAodcoeAAbaCTTstK9xTxYEwRJzRsP3yl1qzx-tgJLSu',
-    currency: 'MXN'
+    currency: 'USD'
+  },
+  algoliaConfig: {
+    apiKey: 'f9697d2eb91d79638d5331b8d644da6a',
+    appId: 'JJH2ZRV4CI',
+    indexName: 'test_ANGULAR7ECOMMERCE',
+    routing: true
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,13 @@ export const environment = {
   production: false,
   paypal: {
     client: 'AesIc2gjvYH0EtI_wEqNMDQCrQsJpZOa-P-QwAodcoeAAbaCTTstK9xTxYEwRJzRsP3yl1qzx-tgJLSu',
-    currency: 'MXN'
+    currency: 'USD'
+  },
+  algoliaConfig: {
+    apiKey: 'f9697d2eb91d79638d5331b8d644da6a',
+    appId: 'JJH2ZRV4CI',
+    indexName: 'test_ANGULAR7ECOMMERCE',
+    routing: true
   }
 };
 
